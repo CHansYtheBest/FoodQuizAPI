f7475b34a2726d0a4954ca6b0685d0cda42fcadd
Nikolay <bukatnikol@gmail.com>
2022-02-18 23:35:15 +0300

    Сделал все категории в html

--- js/navigationFunc.js
+++ js/navigationFunc.js
@@ -4,7 +4,7 @@ const NavFooter = document.querySelector("[data-page]");
 var CurrentPage = NavFooter.dataset.page;
 
 nextButton.addEventListener("click", (e) => {
-  if (NavFooter.dataset.page == 5) {
+  if (NavFooter.dataset.page == 4) {
   } else {
     CurrentPage++;
     NavFooter.dataset.page = CurrentPage;
