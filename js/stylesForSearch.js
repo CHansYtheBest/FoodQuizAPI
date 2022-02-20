3a90240037190cde54314e8d3a07e903d4079ddd
Nikolay <bukatnikol@gmail.com>
2022-02-20 18:21:13 +0300

    Изменил дизайн и посмотрел шрифты, нужно выбрать

--- js/stylesForSearch.js
+++ js/stylesForSearch.js
@@ -20,43 +20,18 @@ function changeCurrentPage(CurrentPage) {
   switch (CurrentPage) {
     case 0:
       circle[0].classList.add("currentPage");
-      step1_H1.classList.add("opacity0Fast");
-      setTimeout(() => {
-        step1_H1.textContent = "Step 1: Select the type of food";
-        step1_H1.classList.remove("opacity0Fast");
-      }, 1000);
       break;
     case 1:
       circle[1].classList.add("currentPage");
-      step1_H1.classList.add("opacity0Fast");
-      setTimeout(() => {
-        step1_H1.classList.remove("opacity0Fast");
-        step1_H1.textContent = "Step 2:";
-      }, 1000);
       break;
     case 2:
       circle[2].classList.add("currentPage");
-      step1_H1.classList.add("opacity0Fast");
-      setTimeout(() => {
-        step1_H1.classList.remove("opacity0Fast");
-        step1_H1.textContent = "Step 3:";
-      }, 1000);
       break;
     case 3:
       circle[3].classList.add("currentPage");
-      step1_H1.classList.add("opacity0Fast");
-      setTimeout(() => {
-        step1_H1.classList.remove("opacity0Fast");
-        step1_H1.textContent = "Step 4:";
-      }, 1000);
       break;
     case 4:
       circle[4].classList.add("currentPage");
-      step1_H1.classList.add("opacity0Fast");
-      setTimeout(() => {
-        step1_H1.classList.remove("opacity0Fast");
-        step1_H1.textContent = "Step 5:";
-      }, 1000);
       break;
     default:
       break;
